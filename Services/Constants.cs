7d495f4f4089dabd1a9d4b6c9f12c8a97d96e785
1walkerit <pavol.walker@gmail.com>
2026-04-29 19:28:27 +0200

    Add GitHub latest release API URL

--- Services/Constants.cs
+++ Services/Constants.cs
@@ -4,4 +4,7 @@ public static class Constants
 {
     public const string GitHubLocalizationUrl =
         "https://raw.githubusercontent.com/JarredSC/Star-Citizen-CZ-lokalizace/main/Localization/english/global.ini";
+
+    public const string GitHubLatestReleaseApi =
+        "https://api.github.com/repos/1walkerit/sc-cestinator-avalonia/releases/latest";
 }
