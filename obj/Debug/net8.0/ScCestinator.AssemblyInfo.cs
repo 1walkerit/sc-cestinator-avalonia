ae7f9de5ede6cfbf324e17300a795d7b077abf5d
1walkerit <pavol.walker@gmail.com>
2026-04-29 15:21:57 +0200

    Work in progress

--- obj/Debug/net8.0/ScCestinator.AssemblyInfo.cs
+++ obj/Debug/net8.0/ScCestinator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScCestinator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.2.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.2.0+cbc76b941247af11fd1f59bc7d6b366f2ab815cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.2.0+c6206510eb510944f7b91cfb8e232004da617fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScCestinator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScCestinator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.2.0.0")]
