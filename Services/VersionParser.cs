9ccd0f3bf0482bfaceec523f5e2e35c9c2513d77
1walkerit <pavol.walker@gmail.com>
2026-05-12 15:49:15 +0200

    Podpora vícepismenného suffixu verze CZ

--- Services/VersionParser.cs
+++ Services/VersionParser.cs
@@ -5,7 +5,7 @@ namespace ScCestinator.Services;
 public static class VersionParser
 {
     // Matches versions like: 0.9y, 0.10, 1.0, 1.2a
-    private static readonly Regex VersionRegex = new(@"\d+\.\d+[a-zA-Z]?", RegexOptions.Compiled);
+    private static readonly Regex VersionRegex = new(@"\d+\.\d+[a-zA-Z]*", RegexOptions.Compiled);
 
     public static string? ParseVersionFromFirstLine(string? firstLine)
     {
